1155d0cf9eb6a4d595874d877afb9bba17ef7d67
khlebnikovroman <khlebnikov.roman187@gmail.com>
2022-01-29 16:17:05 +0300

    initial commit

---
+++ WebAppAbout/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppAbout.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
